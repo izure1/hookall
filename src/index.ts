623bc2e913e5cacba0e53c7fc3dab0d2b711417c
izure <izure@naver.com>
2024-11-13 23:07:18 +0900

    chore: update JSR version

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { useHookall, IHookall } from './Hookall'
-export { useHookallSync, IHookallSync } from './HookallSync'
+export type { IHookall } from './Hookall'
+export type { IHookallSync } from './HookallSync'
+export { useHookall } from './Hookall'
+export { useHookallSync } from './HookallSync'
